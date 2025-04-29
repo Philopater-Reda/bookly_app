c9ca25bd8771afbdc331e342b39e88b9499ce745
Philopater-Reda <philobaterreda20@gmail.com>
2025-04-29 19:42:00 +0300

    refactor: enhance error handling in HomeRepoEmpl and update Failure class

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,1 +1,49 @@
-abstract class Failure {}
+// ignore_for_file: deprecated_member_use
+
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String errMassage;
+ const Failure({this.errMassage = ''});
+}
+class ServerFailure extends Failure {
+  const ServerFailure(String errMassage) : super(errMassage: errMassage);
+  //ServerFailure(super.errMassage);
+  factory ServerFailure.fromDioError(DioError dioError) {
+    switch (dioError.type) {
+      case DioErrorType.connectionTimeout:
+        return const ServerFailure('Connection Timeout');
+      case DioErrorType.sendTimeout:
+        return const ServerFailure('Send Timeout');
+      case DioErrorType.receiveTimeout:
+        return const ServerFailure('Receive Timeout');
+      case DioErrorType.badResponse:
+        return const ServerFailure('Server Error');
+      case DioErrorType.cancel:
+        return const ServerFailure('Request Cancelled');
+      case DioErrorType.unknown:
+        return const ServerFailure('Unknown Error');
+      case DioExceptionType.badCertificate:
+        return const ServerFailure('Bad Certificate');
+      case DioExceptionType.connectionError:
+        return const ServerFailure('Connection Error');
+      
+    }
+  }
+  factory ServerFailure.fromResponse(int statusCode , dynamic response) {
+    switch (statusCode) {
+      case 400:
+        return const ServerFailure('Bad Request');
+      case 401:
+        return const ServerFailure('Unauthorized');
+      case 403:
+        return const ServerFailure('Forbidden');
+      case 404:
+        return const ServerFailure('Not Found');
+      case 500:
+        return const ServerFailure('Internal Server Error');
+      default:
+        return const ServerFailure('Unknown Error');
+    }
+  }
+}
--- lib/features/home/data/repos/home_repo_empl.dart
+++ lib/features/home/data/repos/home_repo_empl.dart
@@ -2,11 +2,38 @@ import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import '../../../../core/utils/api_service.dart';
 
 class HomeRepoEmpl implements HomeRepo {
+  final ApiService apiService;
+  HomeRepoEmpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
+    try {
+      var data = await apiService.get(
+        endPoint:
+            "volumes?Filtering=free-ebooks&Sorting=newest&q=subject : programming",
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(
+          BookModel.fromJson(item),
+        );
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 
   @override
